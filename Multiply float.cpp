29dc16012049c86b852f6439b70ae94d3eff5996
Harsh A Patil <105476362+Harsh-patil28@users.noreply.github.com>
2024-09-10 16:55:07 +0530

    Create Multiply float.cpp

---
+++ Multiply float.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+float mul(float a,float b)
+{
+  return a*b ;
+}
+int main()
+{
+  float a = 1.2 , b = 2.4, product;
+cout << "Product="<<mul(a,b)<<endl;
+return 0;
+}
